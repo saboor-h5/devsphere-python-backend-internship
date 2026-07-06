6b38c96fe123513e51ca9d396c4fb6c8e258faa0
Saboor Hussain <saboorhussain00024@gmail.com>
2026-07-06 20:39:43 +0500

    Accidentally remove Middleware, therefore added it again.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
-from app.routers.features import router
+from fastapi import FastAPI, Request
+from app.routers.features import router as features_router
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    print(f"{request.method} {request.url}")
+    response = await call_next(request)
+    return response
+
 
 @app.get("/")
 def root():
@@ -13,4 +21,4 @@ def about():
     return {"message": "This backend server is built using FastAPI."}
 
 
-app.include_router(router)  
+app.include_router(features_router)
